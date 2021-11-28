e916c32b5eb93debf84bfee4f471422d5375f769
Pierre Glandon <pglandon78@gmail.com>
2021-11-28 23:41:18 +0100

    feat: add formating and linting with eslint_d and prettier

--- lua/lsp/null-ls-config.lua
+++ lua/lsp/null-ls-config.lua
@@ -9,11 +9,17 @@ local sources = {
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.diagnostics.selene,
 
+    -- Typescript
+    null_ls.builtins.diagnostics.eslint_d,
+    null_ls.builtins.code_actions.eslint_d,
+    null_ls.builtins.formatting.eslint_d,
+    null_ls.builtins.formatting.prettier,
+
     -- Refactoring
-     null_ls.builtins.code_actions.refactoring
+    null_ls.builtins.code_actions.refactoring,
 }
 
 null_ls.config({ sources = sources, debug = true })
 require('lspconfig')['null-ls'].setup({
-  on_attach = on_attach
+    on_attach = on_attach,
 })
