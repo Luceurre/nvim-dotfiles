44ac9e16cafba25e2dc2fabbcee258b11d92b55c
Pierre Glandon <pglandon78@gmail.com>
2021-11-28 23:44:04 +0100

    feat: replace prettier with prettierd

--- lua/lsp/null-ls-config.lua
+++ lua/lsp/null-ls-config.lua
@@ -13,7 +13,7 @@ local sources = {
     null_ls.builtins.diagnostics.eslint_d,
     null_ls.builtins.code_actions.eslint_d,
     null_ls.builtins.formatting.eslint_d,
-    null_ls.builtins.formatting.prettier,
+    null_ls.builtins.formatting.prettierd,
 
     -- Refactoring
     null_ls.builtins.code_actions.refactoring,
