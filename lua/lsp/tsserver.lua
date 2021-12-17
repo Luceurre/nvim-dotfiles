b2772c939c08f17b3e2329eea42f646019931a0b
Pierre Glandon <pglandon78@gmail.com>
2021-12-17 01:01:11 +0100

    refactor: extract tsserver config in its own file

---
+++ lua/lsp/tsserver.lua
@@ -0,0 +1,73 @@
+local on_attach = function(client, bufnr)
+    client.resolved_capabilities.document_formatting = false
+    client.resolved_capabilities.document_range_formatting = false
+
+    local ts_utils = require('nvim-lsp-ts-utils')
+
+    ts_utils.setup({
+        debug = true,
+        disable_commands = false,
+        enable_import_on_completion = true,
+
+        -- import all
+        import_all_timeout = 5000, -- ms
+        -- lower numbers = higher priority
+        import_all_priorities = {
+            same_file = 1, -- add to existing import statement
+            local_files = 2, -- git files or files with relative path markers
+            buffer_content = 3, -- loaded buffer content
+            buffers = 4, -- loaded buffer names
+        },
+        import_all_scan_buffers = 100,
+        import_all_select_source = false,
+
+        -- filter diagnostics
+        filter_out_diagnostics_by_severity = {},
+        filter_out_diagnostics_by_code = {},
+
+        -- inlay hints
+        auto_inlay_hints = true,
+        inlay_hints_highlight = 'Comment',
+
+        -- update imports on file move
+        update_imports_on_move = true,
+        require_confirmation_on_move = false,
+        watch_dir = nil,
+    })
+
+    ts_utils.setup_client(client)
+
+    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+    -- no default maps, so you may want to define some here
+    local opts = { silent = true }
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ls', ':TSLspOrganize<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>lr', ':TSLspRenameFile<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>li', ':TSLspImportAll<CR>', opts)
+
+    vim.cmd('autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()')
+end
+
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities.textDocument.completion.completionItem.preselectSupport = true
+capabilities.textDocument.completion.completionItem.insertReplaceSupport = true
+capabilities.textDocument.completion.completionItem.labelDetailsSupport = true
+capabilities.textDocument.completion.completionItem.deprecatedSupport = true
+capabilities.textDocument.completion.completionItem.commitCharactersSupport = true
+capabilities.textDocument.completion.completionItem.tagSupport = { valueSet = { 1 } }
+capabilities.textDocument.completion.completionItem.resolveSupport = {
+    properties = {
+        'documentation',
+        'detail',
+        'additionalTextEdits',
+    },
+}
+
+local opts = {
+    init_options = require('nvim-lsp-ts-utils').init_options,
+    capabilities = capabilities,
+    on_attach = on_attach,
+}
+
+return opts
